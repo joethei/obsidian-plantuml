037ccf673f8654b5b9c17d307f64741098088543
Johannes Theiner <kontakt@joethei.xyz>
2022-04-28 10:56:24 +0200

    updating linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,9 @@ module.exports = {
     plugins: ["@typescript-eslint"],
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
     rules: {
-        "@typescript-eslint/no-unused-vars": [
-            2,
-            { args: "all", argsIgnorePattern: "^_" },
-        ],
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/ban-ts-comment": "off",
-        "@typescript-eslint/no-var-requires": "off"
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-unused-vars": "off"
     },
 };
